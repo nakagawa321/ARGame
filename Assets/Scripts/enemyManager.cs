899140d96cbf8fb525da1347c073496403d9349a
nakagawa321 <54922044+nakagawa321@users.noreply.github.com>
2019-09-09 17:23:34 +0900

    commit
    add enemy&hit

--- Assets/Scripts/enemyManager.cs
+++ Assets/Scripts/enemyManager.cs
@@ -11,13 +11,15 @@ public class enemyManager : MonoBehaviour
     private int enemyOccurrenceCnt = 0;
     private int enemyCreate = 0;
     private int enemyValue = 100;
+    private int createMin = 50, createMax = 200;
+    private float speedMin = 0.01f, speedMax = 0.1f; // 移動スピード
+    private Vector3 ramVector = new Vector3(0.0f, 0.0f, 0.0f);
 
-    public float speed = 0.1f;//移動スピード
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -25,34 +27,35 @@ public class enemyManager : MonoBehaviour
     {
         // 子として作成
         var parent = bambooes.transform;
-        if (/*onClick.startFlg == true &&*/ (enemyOccurrenceCnt % 100) == 0)
+        if (onClick.startFlg == true && (enemyOccurrenceCnt % Random.Range(createMin, createMax)) == 0)
         {
-            GameObject obj = Instantiate(bambooPrehab, new Vector3(0.0f, 0.0f, 10.0f), Quaternion.identity, parent); // prehab作成
-            bamboo[enemyCreate] = GameObject.Find("bamboo(Clone)");
+            // ランダム値取得
+            ramVector.x = Random.Range(-50.0f, 50.0f);
+            ramVector.y = Random.Range(-50.0f, 50.0f);
+            ramVector.x = Random.Range(-50.0f, 50.0f);
+
+            GameObject obj = Instantiate(bambooPrehab, ramVector, Quaternion.identity, parent); // prehab作成
+            obj.name = "bamboo(Clone)" + enemyCreate; // 名前編集
+            bamboo[enemyCreate] = GameObject.Find("bamboo(Clone)" + enemyCreate);
             enemyCreate++;
         }
         enemyOccurrenceCnt++;
 
 
         // 竹を動かす
-        for (int i = 0; i < enemyValue; i++) {
-            if (bamboo != null)
+        for (int i = 0; i < enemyValue; i++)
+        {
+            if (bamboo[i] != null)
             {
+                Debug.Log(i);
                 //targetの方に少しずつ向きが変わる
                 bamboo[i].transform.rotation = Quaternion.Slerp(bamboo[i].transform.rotation, Quaternion.LookRotation(ARCamera.transform.position - bamboo[i].transform.position), 0.3f);
 
                 //targetに向かって進む
-                bamboo[i].transform.position += bamboo[i].transform.forward * speed;
+                bamboo[i].transform.position += bamboo[i].transform.forward * Random.Range(speedMin, speedMax);
             }
         }
 
-    }
-
-
-    // 竹の配列何番目か数字
-    public int getEnemyCreate()
-    {
-        return this.enemyCreate;
     }
 
 }
